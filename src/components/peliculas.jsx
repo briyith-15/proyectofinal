5ee0c8945e90f020114d6db8e8425137526a31db
camilaw <niuobregonsalhuana@crackthecode.la>
2025-10-23 17:26:31 -0500

    subiendo mi proyecto react al repositorio

--- src/components/peliculas.jsx
+++ src/components/peliculas.jsx
@@ -1,21 +1,33 @@
 import datos from '../data/peliculas.json';
+
 function ListaPeliculas() {
-    return(
-        <div className='p-20'>
-            <h1 className='text-center text-3xl fond-bold text-blue mb-8'>lista de peliculas</h1>
-            <div className='grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-              {datos.peliculas.map(peliculas => (
-                <div key={peliculas.id}
-                classNa me='bg-white shadow-md p-4'>
-                    <img src={`./${peliculas.imagen}`} alt="" className='w-full rounded-2xl' />
-                    <h3 className='text-xl font-semibold text-gray-800 mb-2"' >{peliculas.titulo}</h3>
-                    <p className='text-sm text-gray-600"'> Año: {peliculas.año}</p>
-                    <p className='text-sm text-gray-600'>Genero: {peliculas.genero}</p>
-                </div>
-               ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="p-20">
+      <h1 className="text-center text-3xl font-bold text-blue-500 mb-8">
+        Lista de Películas
+      </h1>
+
+      <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {datos.peliculas.map((pelicula) => (
+          <div
+            key={pelicula.id}
+            className="bg-white shadow-md p-4 rounded-xl hover:shadow-lg transition"
+          >
+            <img
+              src={`/proyectofinal/json/${pelicula.imagen}`}
+              alt={pelicula.titulo}
+              className="w-full rounded-2xl"
+            />
+            <h3 className="text-xl font-semibold text-gray-800 mb-2">
+              {pelicula.titulo}
+            </h3>
+            <p className="text-sm text-gray-600">Año: {pelicula.año}</p>
+            <p className="text-sm text-gray-600">Género: {pelicula.genero}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ListaPeliculas;
