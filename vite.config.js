326dbe26b554dd02a16035119d26f303b8c22e03
camilaw <niuobregonsalhuana@crackthecode.la>
2025-10-23 17:04:53 -0500

    subiendo mi proyecto react al repositorio

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/proyectofinal/', // 👈 va aquí, fuera del array
+  base: '/proyectofinal/', // 👈 nombre EXACTO del repositorio en GitHub
 })
